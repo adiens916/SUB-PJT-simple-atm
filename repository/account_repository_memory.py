af95ceee1d49f86239c7b1825a6c28fee18de0a3
Inho <adiens916@naver.com>
2022-11-27 15:56:40 +0900

    [feat] Add account repository by memory

---
+++ repository/account_repository_memory.py
@@ -0,0 +1,27 @@
+from domain.account import Account
+from .account_repository import AccountRepository
+
+
+class AccountRepositoryMemory(AccountRepository):
+    store = dict()
+
+    def save(self, account: Account) -> Account:
+        self.store[account.account_number] = account
+        return account
+
+    def find_by_account_number(self, account_number: str) -> Account | None:
+        account = self.store.get(account_number)
+        if account:
+            return account
+        else:
+            print("The account is not found.")
+            return None
+
+    def update_balance(self, account_number: str, balance: int) -> bool:
+        account = self.find_by_account_number(account_number)
+        if account:
+            account.balance = balance
+            self.store[account_number] = account
+            return True
+        else:
+            return False
