ac09cbf005b2cc87f6dffaa74e19b736741a899f
Inho <adiens916@naver.com>
2022-11-27 15:10:43 +0900

    [feat] Add account & card domains

---
+++ domain/account.py
@@ -0,0 +1,4 @@
+class Account:
+    def __init__(self) -> None:
+        self.account_number = ""
+        self.balance = 0
---
+++ domain/card.py
@@ -0,0 +1,5 @@
+class Card:
+    def __init__(self) -> None:
+        self.card_number = ""
+        self.pin_number = ""
+        self.linked_account_number = ""
