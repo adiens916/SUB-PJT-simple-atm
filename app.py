0828b1fe55c70799fe06327dc734fd93badf4fc5
Inho <adiens916@naver.com>
2022-12-01 06:10:28 +0900

    [refactor] Separate controller and view

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
-import os
-import sys
-
-file = os.path.realpath(__file__)
-parent_dir = os.path.abspath(os.path.join(file, os.pardir))
-root_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
-sys.path.append(root_dir)
-
-
-from service.account_service import AccountService
-from service.card_service import CardService
-
-
-account_service = AccountService()
-card_service = CardService()
-
-account_number = ""
-card_number = ""
+saved_account_number = ""
+saved_card_number = ""
 
 
 def main():
-
     while True:
         print()
         print("< Simple ATM >")
@@ -34,44 +17,31 @@ def main():
         check_auth()
 
         if command == "1":
-            try:
-                balance = account_service.get_balance(account_number)
-                print(f"Current Balance: {balance}")
-            except Exception:
-                print("No such an account.")
+            balance = ""
+            print(f"Current Balance: {balance}")
 
         elif command == "2":
-            try:
-                deposit = int(input())
-                account_service.deposit(account_number, deposit)
-            except Exception:
-                print("Not available input")
+            deposit = int(input())
 
         elif command == "3":
-            try:
-                withdrawal = int(input())
-                account_service.withdraw(account_number, withdrawal)
-            except Exception:
-                print("Not enough money")
+            withdrawal = int(input())
 
         elif command == "4":
             break
 
 
 def check_auth():
-    global account_number
+    global saved_account_number
 
-    if not account_number:
+    if not saved_account_number:
         print("Please enter your account number (including '-').")
         print("If you don't have one, then input 'new' for creating a new account.")
         print("Otherwise, you can return by input 'no'.")
         user_input = input(": ")
 
         if user_input.lower() == "new":
-            account = account_service.create_account()
-            account_number = account.account_number
             print("Now your account has been created!")
-            print("Your account number is", account_number)
+            print("Your account number is", saved_account_number)
 
 
 main()
--- controller/atm_controller.py
+++ controller/atm_controller.py
@@ -14,64 +14,28 @@ from service.card_service import CardService
 account_service = AccountService()
 card_service = CardService()
 
-account_number = ""
-card_number = ""
+
+def get_balance(account_number: str):
+    try:
+        balance = account_service.get_balance(account_number)
+        return f"Current Balance: {balance}"
+    except Exception:
+        return "No such an account."
 
 
-def main():
-
-    while True:
-        print()
-        print("< Simple ATM >")
-
-        print("You can select one of these.")
-        print("- 1. See Balance")
-        print("- 2. Deposit")
-        print("- 3. Withdraw")
-        print("- 4. Exit")
-
-        command = input("Enter a command: ")
-        check_auth()
-
-        if command == "1":
-            try:
-                balance = account_service.get_balance(account_number)
-                print(f"Current Balance: {balance}")
-            except Exception:
-                print("No such an account.")
-
-        elif command == "2":
-            try:
-                deposit = int(input())
-                account_service.deposit(account_number, deposit)
-            except Exception:
-                print("Not available input")
-
-        elif command == "3":
-            try:
-                withdrawal = int(input())
-                account_service.withdraw(account_number, withdrawal)
-            except Exception:
-                print("Not enough money")
-
-        elif command == "4":
-            break
+def deposit(account_number: str, deposit: int):
+    try:
+        deposit = int(input())
+        account_service.deposit(account_number, deposit)
+        return {}
+    except Exception:
+        return "Not available input"
 
 
-def check_auth():
-    global account_number
-
-    if not account_number:
-        print("Please enter your account number (including '-').")
-        print("If you don't have one, then input 'new' for creating a new account.")
-        print("Otherwise, you can return by input 'no'.")
-        user_input = input(": ")
-
-        if user_input.lower() == "new":
-            account = account_service.create_account()
-            account_number = account.account_number
-            print("Now your account has been created!")
-            print("Your account number is", account_number)
-
-
-main()
+def withdraw(account_number: str, withdrawal: int):
+    try:
+        withdrawal = int(input())
+        account_service.withdraw(account_number, withdrawal)
+        return {}
+    except Exception:
+        return "Not enough money"
