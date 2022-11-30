2a1b1a4a73a68a66709d757150ba899288e65339
Inho <adiens916@naver.com>
2022-12-01 06:33:59 +0900

    [feat] Change ATM controller into returning dict

--- controller/atm_controller.py
+++ controller/atm_controller.py
@@ -1,3 +1,4 @@
+# For importing services in other folders
 import os
 import sys
 
@@ -6,36 +7,37 @@ parent_dir = os.path.abspath(os.path.join(file, os.pardir))
 root_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
 sys.path.append(root_dir)
 
-
+# Import services
 from service.account_service import AccountService
 from service.card_service import CardService
-
 
 account_service = AccountService()
 card_service = CardService()
 
-
-def get_balance(account_number: str):
-    try:
-        balance = account_service.get_balance(account_number)
-        return f"Current Balance: {balance}"
-    except Exception:
-        return "No such an account."
+# Import type
+from .atm_controller_type import Response
 
 
-def deposit(account_number: str, deposit: int):
-    try:
-        deposit = int(input())
-        account_service.deposit(account_number, deposit)
-        return {}
-    except Exception:
-        return "Not available input"
+class AtmController:
+    def get_balance(account_number: str) -> Response:
+        try:
+            balance = account_service.get_balance(account_number)
+            return {"data": balance, "ok": "true", "message": ""}
+        except Exception:
+            return {"data": None, "ok": "false", "message": "No such an account."}
 
+    def deposit(account_number: str, deposit: int) -> Response:
+        try:
+            account_service.deposit(account_number, deposit)
+            balance = account_service.get_balance(account_number)
+            return {"data": balance, "ok": "true", "message": "Balance updated."}
+        except Exception:
+            return {"data": None, "ok": "false", "message": "Not available input."}
 
-def withdraw(account_number: str, withdrawal: int):
-    try:
-        withdrawal = int(input())
-        account_service.withdraw(account_number, withdrawal)
-        return {}
-    except Exception:
-        return "Not enough money"
+    def withdraw(account_number: str, withdrawal: int) -> Response:
+        try:
+            account_service.withdraw(account_number, withdrawal)
+            balance = account_service.get_balance(account_number)
+            return {"data": balance, "ok": "true", "message": "Balance updated."}
+        except Exception:
+            return {"data": None, "ok": "false", "message": "Not enough money."}
