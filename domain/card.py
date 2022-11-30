11df4719495c44897f3ec0b3e321ceaa85d148dc
Inho <adiens916@naver.com>
2022-11-30 13:04:06 +0900

    [feat] Add template for card service & domain

--- domain/card.py
+++ domain/card.py
@@ -1,5 +1,18 @@
 class Card:
-    def __init__(self) -> None:
-        self.card_number = ""
-        self.pin_number = ""
-        self.linked_account_number = ""
+    def __init__(self, pin_number: str, linked_account_number=None) -> None:
+        self.card_number = self.assign_card_number()
+        self.hashed_pin_number = self.get_hashed_pin_number(pin_number)
+
+        if isinstance(linked_account_number, str):
+            self.linked_account_number = linked_account_number
+        else:
+            self.linked_account_number = ""
+
+    def assign_card_number(self) -> str:
+        pass
+
+    def validate_card_number(self, card_number: str) -> bool:
+        pass
+
+    def get_hashed_pin_number(self, pin_number: str) -> str:
+        pass
---
+++ service/card_service.py
@@ -0,0 +1,33 @@
+from repository.card_repository_memory import CardRepositoryMemory
+from domain.card import Card
+
+
+class CardService:
+    def __init__(self) -> None:
+        self.repository = CardRepositoryMemory()
+
+    def create_card(self, pin_number: str, linked_account_number=None) -> Card:
+        card = Card(pin_number, linked_account_number)
+        card_saved = self.repository.save(card)
+        return card_saved
+
+    def authenticate_card_by_pin(self, card_number: str, pin_number: str) -> bool:
+        pass
+
+    def get_linked_account_number(self, card_number: str) -> str:
+        pass
+
+    def change_linked_account_number(self, card_number: str, account_number: str):
+        pass
+
+
+class NoSuchElementError(Exception):
+    pass
+
+
+class EmptyValueError(Exception):
+    pass
+
+
+class WrongValueError(Exception):
+    pass
