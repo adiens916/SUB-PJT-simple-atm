b6cefc5e6affae4272d24599d4167fad1066497b
Inho <adiens916@naver.com>
2022-12-01 07:56:21 +0900

    [feat] Add simple console view for interaction

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-saved_account_number = ""
+from controller.atm_controller import AtmController, BankController
+
+atm_controller = AtmController()
+bank_controller = BankController()
+
 saved_card_number = ""
 
 
 def main():
+    print("< Simple ATM >\n")
+
+    prep_account_and_card()
+    authenticate_pin()
+
     while True:
         print()
-        print("< Simple ATM >")
-
         print("You can select one of these.")
         print("- 1. See Balance")
         print("- 2. Deposit")
         print("- 3. Withdraw")
         print("- 4. Exit")
 
-        command = input("Enter a command: ")
-        check_auth()
+        command = input("Enter a command: ").strip()
 
         if command == "1":
             balance = ""
@@ -30,18 +36,53 @@ def main():
             break
 
 
-def check_auth():
-    global saved_account_number
+def prep_account_and_card():
+    global saved_card_number
 
-    if not saved_account_number:
-        print("Please enter your account number (including '-').")
-        print("If you don't have one, then input 'new' for creating a new account.")
-        print("Otherwise, you can return by input 'no'.")
-        user_input = input(": ")
+    print("First, you need a card to use this ATM.")
+    while True:
+        print("Please input a 4-digit PIN number for the card (Ex.: 2573)")
+        pin_number = input(": ").strip()
+        if is_valid_pin_number(pin_number):
+            break
 
-        if user_input.lower() == "new":
-            print("Now your account has been created!")
-            print("Your account number is", saved_account_number)
+    account_number = bank_controller.create_account().get("data")
+    saved_card_number = bank_controller.create_card(pin_number, account_number).get(
+        "data"
+    )
+
+    print("All works done! Now you can use this ATM.\n")
+
+
+def authenticate_pin():
+    available_input_count = 5
+
+    while True:
+        print("Please enter your PIN number.")
+        pin_number = input(": ").strip()
+
+        if not is_valid_pin_number(pin_number):
+            continue
+
+        is_pin_correct = bank_controller.authenticate_card_by_pin(
+            saved_card_number, pin_number
+        ).get("data")
+
+        if is_pin_correct:
+            return
+
+        if available_input_count:
+            print(
+                "Different PIN number... Please retry.", f"({available_input_count}/5)"
+            )
+            available_input_count -= 1
+        else:
+            print("You have used up all input chances. Please contact a nearby bank.")
+            exit()
+
+
+def is_valid_pin_number(pin_number) -> bool:
+    return isinstance(pin_number, str) and len(pin_number) == 4
 
 
 main()
