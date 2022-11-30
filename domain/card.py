58020fa99ba6496d46a80943d192902fcb1ea72d
Inho <adiens916@naver.com>
2022-11-30 13:14:16 +0900

    [feat] Add card number generator & validator

--- domain/card.py
+++ domain/card.py
@@ -1,6 +1,9 @@
+import random
+
+
 class Card:
     def __init__(self, pin_number: str, linked_account_number=None) -> None:
-        self.card_number = self.assign_card_number()
+        self.card_number = self.make_card_number()
         self.hashed_pin_number = self.get_hashed_pin_number(pin_number)
 
         if isinstance(linked_account_number, str):
@@ -8,11 +11,32 @@ class Card:
         else:
             self.linked_account_number = ""
 
-    def assign_card_number(self) -> str:
-        pass
+    def make_card_number(self) -> str:
+        num_strings = list(map(str, range(10)))
+        generate_random_num_string = lambda size: "".join(
+            random.choices(num_strings, k=size)
+        )
 
-    def validate_card_number(self, card_number: str) -> bool:
-        pass
+        card_number_parts = [generate_random_num_string(4) for _ in range(4)]
+        return "-".join(card_number_parts)
 
     def get_hashed_pin_number(self, pin_number: str) -> str:
         pass
+
+    @staticmethod
+    def validate_card_number(card_number: str) -> bool:
+        if not isinstance(card_number, str):
+            return False
+
+        parts = card_number.split("-")
+        if len(parts) != 4:
+            return False
+
+        for i in range(4):
+            if len(parts[i]) != 4:
+                return False
+            for num in parts[i]:
+                if not num.isdigit():
+                    return False
+
+        return True
