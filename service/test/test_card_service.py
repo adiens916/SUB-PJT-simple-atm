b97c9335000978d03924cacd8c02f466c34bfced
Inho <adiens916@naver.com>
2022-11-30 18:16:31 +0900

    [test] Test about linked account

--- service/test/test_card_service.py
+++ service/test/test_card_service.py
@@ -5,6 +5,7 @@ from domain.card import Card
 
 SAMPLE_PIN_NUMBER = "9173"
 SAMPLE_ACCOUNT_NUMBER = "102-301-7523"
+NEW_SAMPLE_ACCOUNT_NUMBER = "101-320-3518"
 
 
 class CardServiceTest(TestCase):
@@ -56,10 +57,49 @@ class CardServiceTest(TestCase):
             result = self.card_service.authenticate_card_by_pin(card.card_number, "123")
 
     def test_get_linked_account_number(self):
-        pass
+        card = self.card_service.create_card(SAMPLE_PIN_NUMBER, SAMPLE_ACCOUNT_NUMBER)
+        account = self.card_service.get_linked_account_number(card.card_number)
+        self.assertEqual(account, SAMPLE_ACCOUNT_NUMBER)
 
     def test_failed_get_linked_account_number_by_empty_account(self):
-        pass
+        card = self.card_service.create_card(SAMPLE_PIN_NUMBER)
+        with self.assertRaises(NoSuchElementError):
+            self.card_service.get_linked_account_number(card.card_number)
+
+    def test_failed_get_linked_account_number_by_empty_card(self):
+        with self.assertRaises(WrongValueError):
+            self.card_service.get_linked_account_number(None)
+            self.card_service.get_linked_account_number("")
 
     def test_change_linked_account_number(self):
+        card = self.card_service.create_card(SAMPLE_PIN_NUMBER, SAMPLE_ACCOUNT_NUMBER)
+        self.card_service.change_linked_account_number(
+            card.card_number, NEW_SAMPLE_ACCOUNT_NUMBER
+        )
+
+        card_changed = self.card_service.repository.find_by_card_number(
+            card.card_number
+        )
+        self.assertEqual(card_changed.linked_account_number, NEW_SAMPLE_ACCOUNT_NUMBER)
+
+    def test_change_linked_account_number_by_empty_account(self):
+        card = self.card_service.create_card(SAMPLE_PIN_NUMBER)
+        self.card_service.change_linked_account_number(
+            card.card_number, NEW_SAMPLE_ACCOUNT_NUMBER
+        )
+
+        card_changed = self.card_service.repository.find_by_card_number(
+            card.card_number
+        )
+        self.assertEqual(card_changed.linked_account_number, NEW_SAMPLE_ACCOUNT_NUMBER)
+
+    def test_failed_change_linked_account_number_by_wrong_account(self):
+        card = self.card_service.create_card(SAMPLE_PIN_NUMBER, SAMPLE_ACCOUNT_NUMBER)
+
+        with self.assertRaises(WrongValueError):
+            self.card_service.change_linked_account_number(
+                card.card_number, "1111111111"
+            )
+
+    def test_failed_change_linked_account_number_by_not_owned_account(self):
         pass
