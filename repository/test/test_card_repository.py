832872f64921d2a7f10d44b63e2cd124b2c5ed28
Inho <adiens916@naver.com>
2022-11-30 14:49:16 +0900

    [test] Fix repository test according to domain

--- repository/test/test_card_repository.py
+++ repository/test/test_card_repository.py
@@ -4,17 +4,13 @@ from domain.card import Card
 
 SAMPLE_CARD_NUMBER = "1234-5678-9012-3456"
 SAMPLE_PIN_NUMBER = "1234"
-SAMPLE_ACCOUNT_NUMBER = "123-456-7890"
 
 
 class CardRepositoryTest(TestCase):
     repository = CardRepositoryMemory()
 
     def setUp(self) -> None:
-        card = Card()
-        card.card_number = SAMPLE_CARD_NUMBER
-        card.pin_number = SAMPLE_PIN_NUMBER
-        card.linked_account_number = SAMPLE_ACCOUNT_NUMBER
+        card = Card(pin_number=SAMPLE_PIN_NUMBER)
         self.card = card
 
     def tearDown(self) -> None:
@@ -22,10 +18,17 @@ class CardRepositoryTest(TestCase):
 
     def test_save(self):
         card_return = self.repository.save(self.card)
-        self.assertIs(card_return.card_number, SAMPLE_CARD_NUMBER)
-        self.assertIs(card_return.pin_number, SAMPLE_PIN_NUMBER)
+
+        card_number_validity = Card.validate_card_number(card_return.card_number)
+        self.assertTrue(card_number_validity)
+
+        self.assertIsNotNone(card_return.hashed_pin_number)
+        self.assertGreater(len(card_return.hashed_pin_number), 0)
 
     def test_find_by_card_number(self):
-        self.repository.save(self.card)
-        card = self.repository.find_by_card_number(SAMPLE_CARD_NUMBER)
-        self.assertEqual(card.card_number, self.card.card_number)
+        card = Card(SAMPLE_PIN_NUMBER)
+        card.card_number = SAMPLE_CARD_NUMBER
+        self.repository.save(card)
+
+        card_ret = self.repository.find_by_card_number(SAMPLE_CARD_NUMBER)
+        self.assertEqual(card.card_number, card_ret.card_number)
