9974ef6546bd9097095afed3bc5338158a16ca75
Inho <adiens916@naver.com>
2022-11-30 11:22:41 +0900

    [feat] Add card repository interface

---
+++ repository/card_repository.py
@@ -0,0 +1,12 @@
+from abc import ABCMeta, abstractmethod
+from domain.card import Card
+
+
+class CardRepository(metaclass=ABCMeta):
+    @abstractmethod
+    def save(self, card: Card) -> Card:
+        pass
+
+    @abstractmethod
+    def find_by_card_number(self, card_number: str) -> Card | None:
+        pass
