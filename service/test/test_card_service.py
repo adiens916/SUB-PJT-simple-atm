e8284acf99135f60d2c4013c31329aa30d1b9546
Inho <adiens916@naver.com>
2022-11-30 17:38:15 +0900

    [test] Move variable out from class

--- service/test/test_card_service.py
+++ service/test/test_card_service.py
@@ -3,31 +3,30 @@ from service.card_service import CardService
 from service.card_service import NoSuchElementError, EmptyValueError, WrongValueError
 from domain.card import Card
 
+SAMPLE_PIN_NUMBER = "9173"
+SAMPLE_ACCOUNT_NUMBER = "102-301-7523"
+
 
 class CardServiceTest(TestCase):
     card_service = CardService()
-
-    def setUp(self) -> None:
-        self.pin_number = "9173"
-        self.account_number = "102-301-7523"
 
     def tearDown(self) -> None:
         self.card_service.repository.store.clear()
 
     def test_create_card(self):
-        card = self.card_service.create_card(self.pin_number, self.account_number)
+        card = self.card_service.create_card(SAMPLE_PIN_NUMBER, SAMPLE_ACCOUNT_NUMBER)
         validity = Card.validate_card_number(card.card_number)
         self.assertTrue(validity)
 
     def test_failed_create_card_by_empty_pin_number(self):
         with self.assertRaises(EmptyValueError):
-            card = self.card_service.create_card(None, self.account_number)
-            card = self.card_service.create_card("", self.account_number)
+            card = self.card_service.create_card(None, SAMPLE_ACCOUNT_NUMBER)
+            card = self.card_service.create_card("", SAMPLE_ACCOUNT_NUMBER)
 
     def test_failed_create_card_by_wrong_pin_number(self):
         with self.assertRaises(WrongValueError):
-            card_short = self.card_service.create_card("324", self.account_number)
-            card_long = self.card_service.create_card("32457", self.account_number)
+            card_short = self.card_service.create_card("324", SAMPLE_ACCOUNT_NUMBER)
+            card_long = self.card_service.create_card("32457", SAMPLE_ACCOUNT_NUMBER)
 
     def test_authenticate_card_by_pin(self):
         pass
