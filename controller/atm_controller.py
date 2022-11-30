e2e896779e39879da5a3659fa497588f6103ba00
Inho <adiens916@naver.com>
2022-11-30 22:20:29 +0900

    [feat] Add simple controller (WIP)

---
+++ controller/atm_controller.py
@@ -0,0 +1,69 @@
+import os
+import sys
+
+file = os.path.realpath(__file__)
+parent_dir = os.path.abspath(os.path.join(file, os.pardir))
+root_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
+sys.path.append(root_dir)
+
+
+from service.account_service import AccountService
+from service.card_service import CardService
+
+
+account_service = AccountService()
+card_service = CardService()
+
+account_number = ""
+card_number = ""
+
+
+def main():
+
+    while True:
+        print()
+        print("< Simple ATM >")
+
+        print("You can select one of these.")
+        print("- 1. See Balance")
+        print("- 2. Deposit")
+        print("- 3. Withdraw")
+        print("- 4. Exit")
+
+        command = input("Enter a command: ")
+        check_auth()
+
+        if command == "1":
+            try:
+                balance = account_service.get_balance(account_number)
+                print(f"Current Balance: {balance}")
+            except Exception:
+                print("No such an account.")
+
+        elif command == "2":
+            try:
+                deposit = int(input())
+                account_service.deposit(account_number, deposit)
+            except Exception:
+                print("Not available input")
+
+        elif command == "3":
+            try:
+                withdrawal = int(input())
+                account_service.withdraw(account_number, withdrawal)
+            except Exception:
+                print("Not enough money")
+
+        elif command == "4":
+            break
+
+
+def check_auth():
+    global account_number
+
+    if not account_number:
+        account = account_service.create_account()
+        account_number = account.account_number
+
+
+main()
